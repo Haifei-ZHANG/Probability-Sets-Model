552e4aebfe91f8cf14bcde6c70adb01dd172951c
Haifei Zhang <zhf_1995@126.com>
2023-08-23 14:30:30 +0200

    calculate weighted average for set size, set acc, rf abstain acc

--- evaluations.py
+++ evaluations.py
@@ -15,14 +15,14 @@ from probability_set_model import ProbabilitySetsModel
 
 
 if __name__ == "__main__":
-    evaluation_criteria = ['RF Acc', 'Determinacy', 'Single Acc', 'Set Acc', 'Set Size', 'U65', 'U80', 'RF Abstention Acc']
+    evaluation_criteria = ['RF Acc', 'Determinacy', 'Single Acc', 'Set Acc', 'Set Size', 'U65', 'U80', 'RF Abstention Acc', 'Best Alpha']
     data_names = ['wine', 'seeds', 'glass', 'ecoli', 'dermatology', 'libras',
                'forest', 'balance_scale', 'vehicle', 'vowel', 'wine_quality', 'segment'] #, 'waveform', 'yeast'
     # data_names = ['glass']
-    dist_types = ['SQE', 'L1', 'KL']
+    dist_types = ['SQE', 'L1', 'KLD']
     prediction_types = ['Maximality', 'E-admissibility']
     nt = 100
-    msl = 3
+    msl = 1
     alpha_list = np.arange(0,20)/20
     K = 10
     total_evaluations = np.zeros((len(evaluation_criteria), len(data_names), 2+len(dist_types)*len(prediction_types))) # n_metrics, n_data, n_models
@@ -33,9 +33,9 @@ if __name__ == "__main__":
         print(data_name)
         data = pd.read_csv('data/{}.csv'.format(data_name))
         X = np.array(data.iloc[:,:-1])
-        y = np.array(data.iloc[:,-1])
+        y = np.array(data.iloc[:,-1]) 
         
-        data_evaluations = np.zeros((K,len(evaluation_criteria), 2+len(dist_types)*len(prediction_types))) # n_fold, n_metrics,n_models 
+        data_evaluations = np.zeros((K, len(evaluation_criteria), 2+len(dist_types)*len(prediction_types))) # n_fold, n_metrics,n_models 
         kf = KFold(n_splits=K, shuffle=False)
         k = -1
         for train_index, test_index in tqdm(kf.split(X)):
@@ -46,9 +46,9 @@ if __name__ == "__main__":
             # NDC and CDM_Vote
             clf = CautiousRandomForest(n_trees=nt, s=2, min_samples_leaf=msl,combination='ndc', random_state=42)
             clf.fit(X_train, y_train)
-            data_evaluations[k,:,0] = np.array(list(clf.score(X_test, y_test).values()))
+            data_evaluations[k,:-1,0] = np.array(list(clf.score(X_test, y_test).values()))
             clf.combination = 'cdm-vote'
-            data_evaluations[k,:,1] = np.array(list(clf.score(X_test, y_test).values()))
+            data_evaluations[k,:-1,1] = np.array(list(clf.score(X_test, y_test).values()))
             
 
             clf = ProbabilitySetsModel(n_trees=nt, alpha=0, p_dist_type='SQE', min_samples_leaf=msl, random_state=42)
@@ -77,24 +77,31 @@ if __name__ == "__main__":
             
                     best_alpha = alpha_list[np.argmax(inner_u65_evaluations)]
                     clf.alpha = best_alpha
-                    data_evaluations[k,:,index] = np.array(list(clf.evaluate(X_test, y_test, prediction_type).values()))
+                    data_evaluations[k,-1,index] = best_alpha
+                    data_evaluations[k,:-1,index] = np.array(list(clf.evaluate(X_test, y_test, prediction_type).values()))
         
-        # np.save('results/imprecise prediction/{}_evaluations.npy'.format(data_name), data_evaluations)           
+        np.save('results/imprecise prediction/min_samples_leaf={}/{}_evaluations.npy'.format(msl, data_name), data_evaluations)           
         for i in range(len(evaluation_criteria)):
-            for j in range(2+len(dist_types)*len(prediction_types)):
-                valid_index = ~np.isnan(data_evaluations[:,i,j])
-                total_evaluations[i,d,j] = data_evaluations[valid_index,i,j].mean()
-    #     np.save('results/imprecise prediction/total_evaluations.npy', total_evaluations)
+            if i in [3,4,7]:
+                for j in range(2+len(dist_types)*len(prediction_types)):
+                    valid_index = ~np.isnan(data_evaluations[:,i,j])
+                    weights = 1 - data_evaluations[valid_index,1,j]
+                    weights /= weights.sum()
+                    total_evaluations[i,d,j] = sum(data_evaluations[valid_index,i,j] * weights)
+            else:
+                for j in range(2+len(dist_types)*len(prediction_types)):
+                    total_evaluations[i,d,j] = data_evaluations[:,i,j].mean()
+        np.save('results/imprecise prediction/min_samples_leaf={}/total_evaluations.npy'.format(msl), total_evaluations)
         print(total_evaluations[:,d,:])
         
         
-    total_evaluations = np.load('results/imprecise prediction/total_evaluations.npy')
+    total_evaluations = np.load('results/imprecise prediction/min_samples_leaf={}/total_evaluations.npy'.format(msl))
     
-    sheet_names = ['RF Acc', 'Determinacy', 'Single Acc', 'Set Acc', 'Set Size', 'U65', 'U80', 'RF Abstention Acc']
+    sheet_names = ['RF Acc', 'Determinacy', 'Single Acc', 'Set Acc', 'Set Size', 'U65', 'U80', 'RF Abstention Acc', 'Best Alpha']
     data_names = ['wine', 'seeds', 'glass', 'ecoli', 'dermatology', 'libras', 'forest', 'balance_scale', 'vehicle', 'vowel', 'wine_quality', 'segment']
     model_names = ['NDC', 'CRF', 'SQE-Max', 'SQE-Ead', 'L1-Max', 'L1-Ead', 'KL-Max', 'KL-Ead']
     
-    with pd.ExcelWriter('results/imprecise prediction/total_evaluations.xlsx') as writer:
+    with pd.ExcelWriter('results/imprecise prediction/min_samples_leaf={}/total_evaluations.xlsx'.format(msl)) as writer:
         for i in range(len(sheet_names)):
             sheet_name = sheet_names[i]
             df = pd.DataFrame(total_evaluations[i].round(4), columns=model_names, index=data_names)
